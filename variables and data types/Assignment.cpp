65e305db81f3da62e68278744d4193943a5c8cff
Prachi-Singh-CSE <prachisingh35045@gmail.com>
2026-06-06 23:53:27 +0530

    added variables and data types chapter

---
+++ variables and data types/Assignment.cpp
@@ -0,0 +1,82 @@
+//  question 1-----------------------------------
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int side;
+//     cout<<"enter side of a sqaure =\n";
+//     cin>>side;
+
+//     int area = side*side;
+//     cout<<"Area of the square ="<<area<<endl;
+//     return 0;
+
+// }
+
+//  question 2----------------------------------
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+    // int pencil;
+    // cout<<"enter the cost of pencil=\n";
+    // cin>>pencil;
+
+    // int pen;
+    // cout<<"enter the cost of pen=\n";
+    // cin>>pen;
+
+    // int eraser;
+    // cout<<"enter the cost of eraser=\n";
+    // cin>>eraser;
+
+//     float total= pencil+pen+eraser;
+//     cout<<"total cost of 3 items = "<<total<<endl;
+//     cout<<"total cost after 18%, GST ="<<total+0.18<<endl;
+     
+//     return 0;
+// }
+
+// question 3 ---------------------------------------
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int principal;
+//     cout<<"enter the principal=\n";
+//     cin>>principal;
+
+//     int rate;
+//     cout<<"enter the rate=\n";
+//     cin>>rate;
+
+//     int time;
+//     cout<<"enter the time=\n";
+//     cin>>time;
+
+//     float SI = (principal*rate*time)/100;
+//     cout<<"simple interest is ="<<SI<<endl;
+
+//     return 0;
+// }
+
+// question 4----------------------------------
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+    
+//     int r ;
+
+//     cout<<"enter the radius of the circle=\n";
+//     cin>>r;
+
+//     float Area;
+//     cout<<"Area of the circle ="<< (22/7*r*r)<<endl;
+//     return 0;
+// }
---
+++ variables and data types/pattern.cpp
@@ -0,0 +1,71 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     cout<<"* * * *\n";
+//     cout<<"* * *\n";
+//     cout<<"* *\n";
+//     cout<<"*\n";
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main (){
+    // int a = 20;
+    // cout<< "a : "<< a <<endl;
+    // return 0;
+
+//     int age  ;
+//     cout<< "enter your age  : ";
+//     cin>> age ;
+//     cout<<"my name is "<<age<<endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int a,b;
+
+//     // input a 
+//     cout << "enter value of a :\n";
+//     cin>> a ;
+//     // input b
+//     cout << "enter value of b :\n";
+//     cin>> b ;
+//     // declare sum 
+//     int sum ;
+//     sum = a+b;
+//     // output
+//     cout<<"sum if a and b is : "<<sum<<endl;
+//     return 0;
+
+// }
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int hindi , english, maths;
+
+    
+    cout << "enter marks of hindi :\n";
+    cin>> hindi ;
+   
+    cout << "enter marks of english :\n";
+    cin>> english ;
+
+    cout<<"enter marks of maths : \n";
+    cin>>maths;
+    
+    // calculate avg 
+    int avg ;
+    avg= (hindi+ english+ maths)/3;
+    // output
+    cout<<"avg marks  : "<<avg<<endl;
+    return 0;
+
+}
