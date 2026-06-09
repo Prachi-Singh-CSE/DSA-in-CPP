eee85d6f35cd7f219b1be6c6288ec3ce69a65743
Prachi-Singh-CSE <prachisingh35045@gmail.com>
2026-06-09 12:39:58 +0530

    added Operators

---
+++ Operators/code.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+      
+//     // implicit conversion 
+
+//     cout<<(10/3)<<endl;
+//     cout<<(10/3.0)<<endl;
+//     cout<<(10.0/3)<<endl;
+//     cout<<('A'+1)<<endl;
+
+//     // explicit conversion
+//     float PI = 3.14;
+//     cout<<(int)(PI)<<endl;
+
+//     return 0;
+// }
+
+// practice questions 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<(bool)3 + 2; // 3 coverts into bool means 1 + 2 = 3 
+    return 0;
+}
